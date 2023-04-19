6e20fffc97b0e68945c1bd9d41085ae05ec5945e
clibc <shirvani.abddullayev@gmail.com>
2023-04-19 23:01:44 +0400

    changes

--- build.bat
+++ build.bat
@@ -1,5 +1,3 @@
 @echo off
 
-pushd build_dir
-    jai.exe ..\src\first.jai -msvc_format -no_cwd -exe first.jai -output_path W:\JaiGame\build_dir\
-popd
+jai.exe ./first.jai -msvc_format -no_cwd -exe gameoflife
