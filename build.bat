b4f72bfb5390074531a664ae3671022ab7ecca2b
clibc <shirvani.abddullayev@gmail.com>
2023-04-19 21:33:34 +0400

    finishing ui work

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
 
 pushd build_dir
-    jai.exe ..\src\first.jai -msvc_format -no_cwd -output_path W:\JaiGame\build_dir\
+    jai.exe ..\src\first.jai -msvc_format -no_cwd -exe first.jai -output_path W:\JaiGame\build_dir\
 popd
